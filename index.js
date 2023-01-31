8da7f100cf81a9cd12f8b9900ad9b9a17cc8a773
cristian <cristiancuello10@gmail.com>
2023-01-31 11:16:34 -0300

    Added mensaje route

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+app.get("/mensaje", (_,res)=>{
+    res.send("Hola desde la ruta mensaje")
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
