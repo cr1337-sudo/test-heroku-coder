2f31a92b2afd7e705fae80ea0d420603405a036f
cristian <cristiancuello10@gmail.com>
2023-01-31 11:46:09 -0300

    Removed routes mensaje & saludo

--- index.js
+++ index.js
@@ -4,11 +4,4 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/mensaje", (_,res)=>{
-    res.send("Hola desde la ruta mensaje")
-})
-app.get("/saludar", (_, res) => {
-  res.send("hola");
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
